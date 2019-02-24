f66bb940536683bcf90ce1b027f0206b73ed1934
GrimReaperSam <fayezlahoud@gmail.com>
2019-02-24 03:23:50 +0100

    Applying operations on tiles and on channels

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 from imageio import imread, imwrite
 from src.pixel_sorting import *
+from src.glitch_operators import *
 
 if __name__ == '__main__':
     image = imread('resources/scene.jpg')
 
-    sort = SortPath(path='circles', path_kwargs={}, key='intensity', interval=40)
-    result = sort.run(image)
+    sort = SortPath(path='random', path_kwargs={}, key='intensity')
+    result = Tile(Channel(sort, [0, ], True), (50, 50)).run(image)
 
     imwrite('results/result.jpg', result)
--- src/glitch_operators.py
+++ src/glitch_operators.py
@@ -1,3 +1,9 @@
+import math
+import random
+
+import numpy as np
+
+
 class Operation:
     def run(self, image):
         pass
@@ -28,4 +34,53 @@ class Chain(Operation):
         result = image
         for operation in self.operations:
             result = operation.run(result)
+        return result
+
+
+class Tile(Operation):
+    def __init__(self, operation, size, density=1., randomize=False):
+        self.operation = operation
+        self.size = size
+        self.density = density
+        self.randomize = randomize
+
+    def run(self, image):
+        result = np.copy(image)
+        width, height = image.shape[:2]
+        tile_width, tile_height = self.size
+
+        i = 0
+        tiles_completed = 0
+        for y in range(0, height, tile_height):
+            for x in range(0, width, tile_width):
+                tiles_completed += 1
+                i += 1
+
+                if self.randomize:
+                    if random.random() > self.density:
+                        continue
+                else:
+                    if self.density == 0 or i < 1.0 / self.density:
+                        continue
+                    else:
+                        i -= 1.0 / self.density
+
+                tile = np.s_[x:x + tile_width, y:y + tile_height, :]
+                result[tile] = self.operation.run(image[tile])
+        return result
+
+
+class Channel(Operation):
+    def __init__(self, operation, channels=None, independent=False):
+        self.operation = operation
+        self.channels = channels if channels is not None else [0, 1, 2]
+        self.independent = independent
+
+    def run(self, image):
+        result = np.copy(image)
+        if self.independent:
+            for ch in self.channels:
+                result[:, :, ch:ch+1] = self.operation.run(image[:, :, ch:ch+1])
+        else:
+            result[:, :, self.channels] = self.operation.run(image[:, :, self.channels])
         return result
