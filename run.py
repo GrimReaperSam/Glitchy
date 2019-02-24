7a7ba5452014e576403b5b7d31143bc74a65692e
GrimReaperSam <fayezlahoud@gmail.com>
2019-02-24 02:30:43 +0100

    Adding paths v2 (Rearrangements!)

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
 from imageio import imread, imwrite
 from src.pixel_sorting import *
-from src.transform import *
 
 if __name__ == '__main__':
     image = imread('resources/scene.jpg')
 
-    sort = SortPath(path='circles', path_kwargs={}, key='hue', interval=50)
+    sort = SortPath(path='circles', path_kwargs={}, key='intensity', interval=40)
     result = sort.run(image)
 
     imwrite('results/result.jpg', result)
--- src/pixel_sorting/interval.py
+++ src/pixel_sorting/interval.py
@@ -19,15 +19,44 @@ def get_intervals(total, interval, progressive=0., randomize=False):
         current += interval
 
 
+def rearrange_keys(keys, mirror=False, splice=0, splice_random=False):
+    if len(keys) == 0:
+        return keys
+    new_keys = list(keys)
+
+    if mirror:
+        get_index = put_index = 0
+        while get_index < len(keys):
+            new_keys[put_index] = keys[get_index]
+            get_index += 1
+            if put_index >= 0:
+                put_index += 1
+            put_index *= -1
+
+    if splice_random:
+        splice_start = random.randrange(len(keys))
+    elif splice > 0:
+        splice_start = int((len(keys) - 1) * splice)
+    else:
+        splice_start = None
+
+    if splice_start is not None:
+        new_keys = new_keys[splice_start:] + new_keys[:splice_start]
+
+    return new_keys
+
+
 class SortPath(Operation):
-    def __init__(self, path='horizontal', path_kwargs=None, key='intensity',
-                 interval=0, progressive=0., randomize=False):
+    def __init__(self, path='horizontal', path_kwargs=None,
+                 interval=0, interval_kwargs=None, key='intensity', keys_kwargs=None):
         self.path = PATHS[path]
         self.path_kwargs = path_kwargs if path_kwargs is not None else {}
-        self.key = KEYS[key]
+
         self.interval = interval
-        self.progressive = progressive
-        self.randomize = randomize
+        self.interval_kwargs = interval_kwargs if interval_kwargs is not None else {}
+
+        self.key = KEYS[key]
+        self.keys_kwargs = keys_kwargs if keys_kwargs is not None else {}
 
     def run(self, image):
         size = image.shape[:2]
@@ -40,10 +69,12 @@ class SortPath(Operation):
             if items.size == 0:
                 continue
             jump = self.interval if self.interval != 0 else items.shape[0]
-            intervals = get_intervals(items.shape[0], jump, self.progressive, self.randomize)
+            intervals = get_intervals(items.shape[0], jump, **self.interval_kwargs)
             for s in intervals:
                 subpart = image[items[s, 0], items[s, 1]]
                 subpart_key = keys[items[s, 0], items[s, 1]]
+
+                subpart_key = rearrange_keys(subpart_key, **self.keys_kwargs)
 
                 indices = np.argsort(subpart_key)
                 result[items[s, 0], items[s, 1]] = subpart[indices, :]
